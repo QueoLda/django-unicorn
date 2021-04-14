05a2b0f9a0e2ca4cc313793392a6a57085643582
Carlos Martinez <carlos@queo.pt>
2021-04-14 10:34:13 +0100

    setup.py corrected

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name="django-unicorn", version="0.26.0")
+setup(
+    name="django-unicorn",
+    version="0.26.0",
+    description="A magical full-stack framework for Django.",
+    authors=["Adam Hill <unicorn@adamghill.com>"],
+    license="MIT",
+    readme="README.md",
+    repository="https://github.com/adamghill/django-unicorn/",
+    homepage="https://www.django-unicorn.com",
+    keywords=["django", "python", "javascript", "fullstack"],
+    install_requires=[
+        "python^3.6",
+        "django>=2.2",
+        "beautifulsoup4>=4.8.0",
+        "orjson^3.2.1",
+        "shortuuid^1.0.1",
+        "cachetools^4.1.1",
+        "dataclasses^0.8.0",
+        "decorator^4.4.2",
+    ],
+)
